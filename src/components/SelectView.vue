84ae0dadfe2016bb5644fb84f959ffd6bb52c17c
zhoucheng <zhoucheng133@outlook.com>
2025-11-02 01:18:53 +0800

    添加primevue依赖

--- src/components/SelectView.vue
+++ src/components/SelectView.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="select_bg">
-    <div class="label">拖拽文件/目录到这里</div>
+    <Button class="select_button" icon="pi pi-plus" rounded/>
+    <div class="label">添加或拖拽文件/目录到这里</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Button } from 'primevue';
 </script>
 
 <style scoped>
+.label{
+  font-size: 15px;
+}
+.select_button{
+  margin-bottom: 15px;
+}
 .select_bg{
   height: 100vh;
   width: 100vw;
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,39 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from 'pinia';
 import "./style/main.css";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from "@primeuix/themes";
+import 'primeicons/primeicons.css'
 
 const pinia = createPinia()
+const color="cyan";
 
-createApp(App)
-.use(pinia)
-.mount("#app");
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: `{${color}.50}`,
+      100: `{${color}.100}`,
+      200: `{${color}.200}`,
+      300: `{${color}.300}`,
+      400: `{${color}.400}`,
+      500: `{${color}.500}`,
+      600: `{${color}.600}`,
+      700: `{${color}.700}`,
+      800: `{${color}.800}`,
+      900: `{${color}.900}`,
+      950: `{${color}.950}`
+    }
+  }
+});
+
+const app = createApp(App);
+
+app.use(pinia)
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset
+  }
+});
+
+app.mount('#app')
