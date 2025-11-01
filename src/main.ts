2a69b775e358a298545e59f1862ad9857e2ec121
zhoucheng <zhoucheng133@outlook.com>
2025-11-01 21:09:05 +0800

    修改默认字体

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from 'pinia';
+import "./style/main.css";
 
-createApp(App).mount("#app");
+const pinia = createPinia()
+
+createApp(App)
+.use(pinia)
+.mount("#app");
