ccacd31e81b33917d3a858355370aad3945d0ce6
zhoucheng <zhoucheng133@outlook.com>
2025-11-01 21:13:39 +0800

    添加拖拽文件的提示

--- src/components/SelectView.vue
+++ src/components/SelectView.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    Select!!
+  <div class="select_bg">
+    拖拽文件/目录到这里
   </div>
 </template>
 
 <script lang="ts" setup>
 </script>
+
+<style scoped>
+.select_bg{
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
