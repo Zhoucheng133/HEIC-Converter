d0cbaafcb17ec2f2e0140698176f58da6e13041c
zhoucheng <zhoucheng133@outlook.com>
2025-11-02 02:27:20 +0800

    添加列表页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <SelectView v-if="store().path.length==0" />
+  <ListView v-else />
 </template>
 
 <script setup lang="ts">
 import store from './store';
 import SelectView from './components/SelectView.vue';
+import ListView from './components/ListView.vue';
 
 
 </script>
