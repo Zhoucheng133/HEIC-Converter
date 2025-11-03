c640e779632e394821365a018c98a519e014d615
zhoucheng <zhoucheng133@outlook.com>
2025-11-03 18:29:27 +0800

    添加执行转换函数

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,51 @@
+import { Command } from "@tauri-apps/plugin-shell";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+
+enum ConvertStatus{
+  wait,
+  done,
+  exist,
+  error,
+}
+
+interface TaskItem{
+  path: string,
+  status: ConvertStatus,
+}
+
 export default defineStore("store", ()=>{
-  let path=ref("");
-  return { path };
+  let files=ref<TaskItem[]>([]);
+  return { files };
 })
+
+export async function runConvert(filePath: string, outputPath: string, override: boolean, { quality=80 }, exif: boolean): Promise<ConvertStatus> {
+
+  let args=[
+    filePath,
+    outputPath,
+    "--quality",
+    quality,
+  ]
+
+  if(override){
+    args.push("--override");
+  }
+
+  if(!exif){
+    args.push("--no-exif");
+  }
+
+  const command = Command.sidecar("binaries/core", [
+    filePath,
+    outputPath,
+  ])
+  const output = await command.execute();
+  if(output.stdout.includes("Skipped")){
+    return ConvertStatus.exist;
+  }else if(output.stdout.includes("Failed")){
+    return ConvertStatus.error;
+  }
+  
+  return ConvertStatus.done;
+}
