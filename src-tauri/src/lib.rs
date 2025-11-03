9642026e3544e8e89150837ed706de8c871c1b6f
zhoucheng <zhoucheng133@outlook.com>
2025-11-03 18:20:40 +0800

    更新扫描路径操作

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,27 +1,51 @@
-use std::fs;
+use std::{fs, path::Path};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn check_path(path: &str) -> String {
-    if let Ok(metadata) = fs::metadata(path) {
-        if metadata.is_dir() {
-            "dir".into()
-        } else if metadata.is_file() {
-            "file".into()
-        } else {
-            "Unkown path".into()
+fn resolve_files(paths: Vec<String>) -> Vec<String> {
+    let mut heic_files = Vec::new();
+
+    for path_str in paths {
+        let path = Path::new(&path_str);
+
+        if path.is_file() {
+            if let Some(ext) = path.extension().and_then(|e| e.to_str()) {
+                if ext.eq_ignore_ascii_case("heic") {
+                    heic_files.push(path_str.clone());
+                }
+            }
+        } else if path.is_dir() {
+            if let Ok(entries) = fs::read_dir(path) {
+                for entry in entries.flatten() {
+                    let entry_path = entry.path();
+                    if entry_path.is_file() {
+                        if let Some(ext) = entry_path.extension().and_then(|e| e.to_str()) {
+                            if ext.eq_ignore_ascii_case("heic") {
+                                if let Some(path_str) = entry_path.to_str() {
+                                    heic_files.push(path_str.to_string());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-    } else {
-        "File or directory not exist".into()
     }
+
+    if heic_files.is_empty() {
+        return vec!["".to_string()];
+    }
+
+    return heic_files
 }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![check_path])
+        .invoke_handler(tauri::generate_handler![resolve_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
